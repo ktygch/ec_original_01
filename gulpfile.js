09335f46f6fd74710e281828b7e13f067e0f06e0
kitayuguchiryuhei <kitayuguchiryuhei@MacBook-Pro.local>
2017-10-02 23:39:22 +0900

    ec_original_01

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+//var rename = require("gulp-rename");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+
+gulp.task("js", function(){
+    gulp.src(["js/**/*.js", "!js/min/**/*.js"])
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest("./html/js/min"));
+});
+
+gulp.task("sass", function(){
+    gulp.src("sass/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest("./html/css"));
+});
+
+gulp.task("default", function(){
+    gulp.watch(["js/**/*.js", "!js/min/**/*.js"],["js"]);
+    gulp.watch("sass/**/*.scss", ["sass"]);
+    gulp.watch(["ejs/**/*.ejs", "!ejs/**/_*.ejs"],["ejs"]);
+});
