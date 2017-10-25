d674d28c0f4fadf0ba7a239d10507e4f6d9047b2
kitayuguchiryuhei <kitayuguchiryuhei@MacBook-Pro.local>
2017-10-26 08:59:45 +0900

    背景色、背景画像を変更できる仕様へ

--- functions.php
+++ functions.php
@@ -4,3 +4,9 @@ add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+$custom_background_defaults = array(
+        'default-color' => 'efefef',
+        'default-image' => get_bloginfo('template_url') . '/images/bg_wall.gif',
+);
+add_theme_support( 'custom-background', $custom_background_defaults );
